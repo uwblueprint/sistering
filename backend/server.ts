a7399eb06c70655c73d7ea2f3e68f96e3e7de4f5
albertlai431 <albertlai631@outlook.com>
2021-10-03 08:45:32 -0400

    Instantiate prisma client

--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import * as firebaseAdmin from "firebase-admin";
 
 import { ApolloServer } from "apollo-server-express";
 import { sequelize } from "./models";
+import { PrismaClient } from "@prisma/client";
 import schema from "./graphql";
 
 const CORS_ALLOW_LIST = ["http://localhost:3000"];
@@ -39,6 +40,25 @@ server.applyMiddleware({
 
 const eraseDatabaseOnSync = false;
 sequelize.sync({ force: eraseDatabaseOnSync });
+
+//prisma
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$connect();
+  console.log("Successfully connected to DB using Prisma!");
+}
+
+main()
+  .catch((e) => {
+    // e should be a PrismaClientInitializationError if connection fails
+    console.error("Error connecting to DB using Prisma!");
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  })
+
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.applicationDefault(),
