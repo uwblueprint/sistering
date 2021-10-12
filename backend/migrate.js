2edc58a63f1de4852e948188cd2113bfcdbb8388
Daniel Yu <dsyu@uwaterloo.ca>
2021-10-11 23:39:52 -0400

    Fix linter errors

--- backend/migrate.js
+++ backend/migrate.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require("ts-node/register");
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require("./umzug").migrator.runAsCLI();
