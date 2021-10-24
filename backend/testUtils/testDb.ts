56b1150c7e52db511cce641fc131da7c1756a261
briantu <brianptu2@gmail.com>
2021-10-23 21:06:08 -0400

    removed sequelize testDb.ts

--- backend/testUtils/testDb.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from "path";
-
-import { Sequelize } from "sequelize-typescript";
-
-/* eslint-disable-next-line import/prefer-default-export */
-export const testSql = new Sequelize(
-  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.POSTGRES_DB_TEST}`,
-  { models: [resolve(__dirname, "../models/*.model.ts")], logging: false },
-);
