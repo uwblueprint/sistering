bd629a8d8fd4c8d188305ce57d7425cb28f9b70c
albertlai431 <albertlai631@outlook.com>
2021-10-27 10:52:55 -0400

    Add GraphQL resolvers for PrerequisitesService

---
+++ backend/graphql/resolvers/prerequisiteResolvers.ts
@@ -0,0 +1,42 @@
+import PrerequisiteService from "../../services/implementations/prerequisiteService";
+import IPrerequisiteService from "../../services/interfaces/IPrerequisiteService";
+import { PrerequisiteRequestDTO, PrerequisiteResponseDTO } from "../../types";
+
+const prerequisiteService: IPrerequisiteService = new PrerequisiteService();
+
+const prerequisiteResolvers = {
+  Query: {
+    prerequisite: async (
+      _parent: undefined,
+      { id }: { id: string },
+    ): Promise<PrerequisiteResponseDTO> => {
+      return prerequisiteService.getPrerequisite(id);
+    },
+    prerequisites: async (): Promise<PrerequisiteResponseDTO[]> => {
+      return prerequisiteService.getPrerequisites();
+    },
+  },
+  Mutation: {
+    createPrerequisite: async (
+      _parent: undefined,
+      { prerequisite }: { prerequisite: PrerequisiteRequestDTO },
+    ): Promise<PrerequisiteResponseDTO> => {
+      const newPrerequisite = await prerequisiteService.createPrerequisite(prerequisite);
+      return newPrerequisite;
+    },
+    updatePrerequisite: async (
+      _parent: undefined,
+      { id, prerequisite }: { id: string; prerequisite: PrerequisiteRequestDTO },
+    ): Promise<PrerequisiteResponseDTO | null> => {
+      return prerequisiteService.updatePrerequisite(id, prerequisite);
+    },
+    deletePrerequisite: async (
+      _parent: undefined,
+      { id }: { id: string },
+    ): Promise<void> => {
+      return prerequisiteService.deletePrerequisite(id);
+    },
+  },
+};
+
+export default prerequisiteResolvers;
