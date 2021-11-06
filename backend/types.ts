eb6cb670adc638a62aded1e4bf750f6c9890482e
albertlai431 <albertlai631@outlook.com>
2021-11-06 14:01:19 -0400

    Modify schema and create types
    Co-authored-by: Brian Tu <brianptu2@gmail.com>

--- backend/types.ts
+++ backend/types.ts
@@ -13,6 +13,29 @@ export type UserDTO = {
   role: Role;
 };
 
+export type PostingDTO = {
+  id: string;
+  branchId: string;
+  //TODO
+  // branch: Branch;
+  // shifts: Shift[];
+  // skills          PostingOnSkill[]
+  // employee        PostingOnEmployeePoc[]
+  title: string;
+  type: PostingType;
+  description: string;
+  startDate: Date;               
+  endDate: Date;               
+  autoClosingDate: Date;               
+  numVolunteers: number;                    
+};
+
+export type PostingRequestDTO = Omit<PostingDTO, "id">;
+
+export type PostingResponseDTO = PostingDTO;
+
+//here
+
 export type CreateUserDTO = Omit<UserDTO, "id"> & { password: string };
 
 export type UpdateUserDTO = Omit<UserDTO, "id">;
@@ -22,6 +45,8 @@ export type RegisterUserDTO = Omit<CreateUserDTO, "role">;
 export type AuthDTO = Token & UserDTO;
 
 export type Letters = "A" | "B" | "C" | "D";
+
+export type PostingType = "INDIVIDUAL" | "GROUP";
 
 export type NodemailerConfig = {
   service: "gmail";
