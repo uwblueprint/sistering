fae8d825abe40d1f11d3091586ffe6b46f3649d6
Sherry Li <sherryhli@users.noreply.github.com>
2021-11-28 23:29:01 -0500

    Remove unused files

--- backend/migrate.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require("ts-node/register");
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("./umzug").migrator.runAsCLI();
