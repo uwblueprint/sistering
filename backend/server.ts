4aabd1bcb0c07574caa0dd5420e681b7b8b5d46c
Sherry Li <sherryhli@users.noreply.github.com>
2021-12-14 22:17:19 -0500

    Set up backend and frontend deployment with CI (#66)
    * Set up backend deployment to Heroku
    
    * Set up frontend deployment to Firebase Hosting
    
    * Change Heroku branch name
    
    * Set Node.js version to 14.15.5 in lint workflow
    
    * Ignore build folders when linting
    
    * Split Firebase service account credential creation into separate step
    
    * Un-ignore credentials before pushing to Heroku
    
    * Trivial commit to force backend rebuild
    
    * Localize working-directory setting in backend deploy action
    
    * Modify Firebase admin initialization
    
    * Remove firebaseServiceAccount.json
    
    * Remove unused import in backend/graphql/index.ts
    
    * Update package names
    
    * Force push to Heroku
    
    * Alert Google Login error message in frontend
    
    * Remove pull_request trigger for Heroku deploys

--- backend/server.ts
+++ backend/server.ts
@@ -6,7 +6,12 @@ import * as firebaseAdmin from "firebase-admin";
 import { ApolloServer } from "apollo-server-express";
 import schema from "./graphql";
 
-const CORS_ALLOW_LIST = ["http://localhost:3000"];
+const CORS_ALLOW_LIST = [
+  "http://localhost:3000",
+  "https://sistering-dev.firebaseapp.com",
+  "https://sistering-dev.web.app",
+  /^https:\/\/sistering-dev--pr.*\.web\.app$/,
+];
 
 const CORS_OPTIONS: cors.CorsOptions = {
   origin: CORS_ALLOW_LIST,
@@ -36,10 +41,17 @@ server.applyMiddleware({
 });
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.applicationDefault(),
+  credential: firebaseAdmin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_SVC_ACCOUNT_PRIVATE_KEY?.replace(
+      /\\n/g,
+      "\n",
+    ),
+    clientEmail: process.env.FIREBASE_SVC_ACCOUNT_CLIENT_EMAIL,
+  }),
 });
 
-app.listen({ port: 5000 }, () => {
+app.listen({ port: process.env.PORT || 5000 }, () => {
   /* eslint-disable-next-line no-console */
-  console.info("Server is listening on port 5000!");
+  console.info(`Server is listening on port ${process.env.PORT || 5000}!`);
 });
