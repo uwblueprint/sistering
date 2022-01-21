f42bae7fca94363fb63c18a4ff939003d56a55ac
Albert Lai <albertlai631@outlook.com>
2022-01-20 20:36:59 -0500

    Create Volunteer Posting List Page (#96)
    * Add Volunteer Postings Page component
    
    * Add Volunteer PostingsPage routes
    
    * Run linter

---
+++ frontend/src/components/pages/volunteer/posting/VolunteerPostingsPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const VolunteerPostingsPage = (): React.ReactElement => {
+  return (
+    <div>
+      <Text textStyle="display-large">Volunteer Postings</Text>
+    </div>
+  );
+};
+
+export default VolunteerPostingsPage;
