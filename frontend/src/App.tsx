02242701cd4a931d0ee6b0472bca6668b6dabf55
briantu <brianptu2@gmail.com>
2022-01-30 11:26:15 -0500

    add Open Sans font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "@fontsource/inter/400.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/raleway/400.css";
 import "@fontsource/raleway/600.css";
+import "@fontsource/open-sans/500.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import React, { useState, useReducer } from "react";
