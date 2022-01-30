dcb37a67c726fbd624704ca12da40f55a317a411
Jiaru Cao <jiaru.cao@gmail.com>
2022-01-30 15:55:24 -0500

    Remove userId field for employee model

---
+++ backend/prisma/migrations/20220130202413_remove_user_id_field_for_employee_model/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `employees` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "employees" DROP CONSTRAINT "employees_branch_id_fkey";
+
+-- DropIndex
+DROP INDEX "employees_id_key";
+
+-- DropIndex
+DROP INDEX "employees_userId_key";
+
+-- DropIndex
+DROP INDEX "volunteers_id_key";
+
+-- AlterTable
+ALTER TABLE "employees" DROP COLUMN "userId";
+
+-- AddForeignKey
+ALTER TABLE "employees" ADD CONSTRAINT "employees_branch_id_fkey" FOREIGN KEY ("branch_id") REFERENCES "branches"("id") ON DELETE CASCADE ON UPDATE CASCADE;
