d17593ea518906e96197a6502d43c3d100046dad
hujoseph99 <hujoseph99@gmail.com>
2022-02-03 18:13:40 -0500

    update migrations to be up to date with main

--- backend/prisma/migrations/20220203230753_add_pk_and_note_field_to_signups/migration.sql
+++ backend/prisma/migrations/20220203230753_add_pk_and_note_field_to_signups/migration.sql
@@ -3,9 +3,11 @@
 
   - The primary key for the `signups` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `id` on the `signups` table. All the data in the column will be lost.
+  - Added the required column `note` to the `signups` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "signups" DROP CONSTRAINT "signups_pkey",
 DROP COLUMN "id",
+ADD COLUMN     "note" TEXT NOT NULL,
 ADD CONSTRAINT "signups_pkey" PRIMARY KEY ("shifts_id", "userId");
