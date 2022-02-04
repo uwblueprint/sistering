ec6ac5779b86b0af874e5def9653af0a98697f39
briantu <brianptu2@gmail.com>
2022-02-03 19:34:44 -0500

    remove Open Sans font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import "@fontsource/inter/700.css";
 import "@fontsource/raleway/400.css";
 import "@fontsource/raleway/500.css";
 import "@fontsource/raleway/600.css";
-import "@fontsource/open-sans/500.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import React, { useState, useReducer } from "react";
