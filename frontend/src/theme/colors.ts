479d4005b785445009e5344b0b22c4af1d4f1099
LenaNguyen27 <nguyen.lena27@hotmail.com>
2022-02-04 23:00:18 -0500

    lint & fix colours

--- frontend/src/theme/colors.ts
+++ frontend/src/theme/colors.ts
@@ -1,22 +1,21 @@
 const colors = {
-	brand: 
-	{
-	  50: '#f0e3ff',
-	  100: '#cfb2ff',
-	  200: '#b27fff',
-	  300: '#974cff',
-	  400: '#811aff',
-	  500: '#7000e6',
-	  600: '#4b00b4',
-	  700: '#2d0082',
-	  800: '#150050',
-	  900: '#050020',
-	},
+  brand: {
+    50: "#f0e3ff",
+    100: "#cfb2ff",
+    200: "#b27fff",
+    300: "#974cff",
+    400: "#811aff",
+    500: "#7000e6",
+    600: "#4b00b4",
+    700: "#2d0082",
+    800: "#150050",
+    900: "#050020",
+  },
   violet: "#7000E6",
   teal: "#00D2D2",
   green: {
     light: "#DFE9B6",
-    dark: "#949F6A",
+    dark: "#3C4B00",
   },
   background: {
     white: "#FFFFFF",
@@ -24,7 +23,7 @@ const colors = {
     dark: "#E7E7E7",
   },
   text: {
-    default: "#1A1A1A",
+    default: "#333333",
     white: "#FFFFFF",
     gray: "#868686",
   },
