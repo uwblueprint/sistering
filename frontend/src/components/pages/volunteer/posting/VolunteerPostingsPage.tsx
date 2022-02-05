0f0392d55b4dd85e2ed97df7b0ef4dc4aab79c46
Boya <boyazhang123@gmail.com>
2022-02-05 14:53:56 -0800

    refactor types and data fetching

--- frontend/src/components/pages/volunteer/posting/VolunteerPostingsPage.tsx
+++ frontend/src/components/pages/volunteer/posting/VolunteerPostingsPage.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import { gql, useApolloClient, useQuery } from "@apollo/client";
 import { Text, Box, HStack, Select } from "@chakra-ui/react";
 import PostingCard from "./PostingCard";
@@ -7,63 +7,79 @@ import { PostingResponseDTO } from "../../../../types/PostingTypes";
 const postingArr = [{
   id: "1",
   skills: [{id: '1', name:"CPR"}, {id: '2', name: "WHMIS"}, {id: '3', name: "Photocopying"}], 
+  branch: {
+    id: '1', 
+    name: 'sample text'
+  },
   title: "Medical Reception Volunteer",
   startDate: new Date('December 17, 2021'),
   endDate: new Date("Monday, January 20"),
-  autoClosingDate: "Monday, November 30", 
+  autoClosingDate: new Date("Monday, November 30"), 
   isSignedUp: false,
   description: "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis."
 }, 
 {
   id: '2',
   skills: [{id: '1', name:"CPR"}, {id: '2', name: "WHMIS"}, {id: '3', name: "Photocopying"}], 
+  branch: {
+    id: '1', 
+    name: 'sample text'
+  },
   title: "Medical Reception Volunteer",
   startDate: new Date('December 17, 2021'),
   endDate: new Date("Monday, January 20"),
-  autoClosingDate: "Monday, November 30", 
+  autoClosingDate: new Date("Monday, November 30"), 
   isSignedUp: false,
   description: "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis."
 }, 
 {
   id: '3',
   skills: [{id: '1', name:"CPR"}, {id: '2', name: "WHMIS"}, {id: '3', name: "Photocopying"}], 
+  branch: {
+    id: '1', 
+    name: 'sample text'
+  },
   title: "Medical Reception Volunteer",
   startDate: new Date('December 17, 2021'),
   endDate: new Date("Monday, January 20"),
-  autoClosingDate: "Monday, November 30", 
+  autoClosingDate: new Date("Monday, November 30"), 
   isSignedUp: false,
   description: "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis."
 }, 
 ]
+
+const POSTINGS = gql`
+query VolunteerPostingsPage_postings {
+  postings {
+    id
+    skills
+    employees
+    title
+    type
+    status
+    description
+    startDate
+    endDate
+    autoClosingDate
+    numVolunteers
+  }
+}`;
+
 const VolunteerPostingsPage = (): React.ReactElement => {
 
   const [postings, setPostings] = useState<PostingResponseDTO[] | null>(null);
   // how does this work??
-  const POSTINGS = gql`
-      query VolunteerPostingsPage_postings {
-        postings {
-          id
-          skills
-          employees
-          title
-          type
-          status
-          description
-          startDate
-          endDate
-          autoClosingDate
-          numVolunteers
-        }
-  }`;
 
   useQuery(POSTINGS, {
     fetchPolicy: "cache-and-network",
     onCompleted: (data) => setPostings(data)
     });
 
+    console.log(postings)
+
   // const {loading, error, postings} = useQuery(GET_POSTINGS)
   return (
-    <Box bg="#D8AEFF" pt={8}>
+    <Box bg="#D8AEFF" pt={8} minHeight="100vh">
     <Box maxW="1280px" mx='auto'>
       <HStack mt={8} mb={6} mx={1} justify="space-between" bg="#F4F4F4">
         <Text fontSize="xl" fontWeight="600" >Events</Text>
@@ -76,7 +92,7 @@ const VolunteerPostingsPage = (): React.ReactElement => {
           </Select>
           </HStack>
       </HStack>
-      {postings.map((posting) => (
+      {postingArr?.map((posting) => (
         <PostingCard 
         key={posting.id}
         id={posting.id}
@@ -85,8 +101,8 @@ const VolunteerPostingsPage = (): React.ReactElement => {
         startDate={posting.startDate} 
         endDate={posting.endDate} 
         autoClosingDate={posting.autoClosingDate}
-        isSignedUp={posting.isSignedUp}
         description={posting.description}
+        branchName={posting.branch.name}
         />
       ))}
     </Box>
