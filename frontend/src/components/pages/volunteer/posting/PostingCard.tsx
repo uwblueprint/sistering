220d537e4d213f5b63bf1aeea6c4d9106cf69735
Boya <boyazhang123@gmail.com>
2022-02-06 15:11:05 -0800

    change testing data

--- frontend/src/components/pages/volunteer/posting/PostingCard.tsx
+++ frontend/src/components/pages/volunteer/posting/PostingCard.tsx
@@ -11,7 +11,7 @@ const PostingCard = ({id, title, skills, description, startDate, endDate, autoCl
         <Tag px={2} borderRadius="100px" variant="subtle" bg="green.light" color="green.dark" fontSize='12' fontWeight={400} textTransform="inherit">{branchName}</Tag>
         <Text textStyle="heading" fontSize='lg' lineHeight="lg">{title}</Text>
         <HStack spacing={4}>
-          {/* toAdd: conditionally displaying time for event listings*/}
+          {/* toAdd: conditionally displaying time for event listings */}
           <Text fontSize='12'><TimeIcon w={4} pr={1}/>See Posting Details</Text>
           <Text fontSize='12'><CalendarIcon w={4} pr={1}/>{`${startDate.toLocaleDateString('en-US', { weekday: 'long', month:'long', day: '2-digit'})} - ${endDate.toLocaleDateString('en-US', { weekday: 'long', month:'long', day: '2-digit'})}`}</Text>
         </HStack>
