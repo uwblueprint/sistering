e5ad6ca4c68a8b68b5cc71f83ef7ced208fcc13d
Boya <boyazhang123@gmail.com>
2022-02-06 13:31:15 -0800

    complete posting card design

--- frontend/src/components/pages/volunteer/posting/PostingCard.tsx
+++ frontend/src/components/pages/volunteer/posting/PostingCard.tsx
@@ -3,28 +3,30 @@ import { TimeIcon, CalendarIcon} from '@chakra-ui/icons';
 import {Text, Box, VStack, HStack, Badge, Divider, Button, ButtonGroup, Tag} from "@chakra-ui/react";
 import {PostingCardProps } from "../../../../types/PostingTypes";
 
-const PostingCard = ({id, title, skills, description, startDate, endDate, autoClosingDate, branchName} :PostingCardProps) => {
+const PostingCard = ({id, title, skills, description, startDate, endDate, autoClosingDate, branchName, type} :PostingCardProps) => {
+
   return (
     <Box m={1} mb={5} bg='white' borderRadius={2} borderColor=" #E7E7E7">
       <VStack p={6} align="flex-start" fontSize="xs" spacing='1.5'> 
         {/* how to tell type of event? */}
-        <Tag px={2} borderRadius="100px" variant="subtle" bg="#DFE9B6" color="#949F6A" fontSize='12' fontWeight={400} textTransform="inherit">{branchName}</Tag>
+        <Tag px={2} borderRadius="100px" variant="subtle" bg="green.light" color="green.dark" fontSize='12' fontWeight={400} textTransform="inherit">{branchName}</Tag>
         <Text textStyle="heading" fontSize='lg' lineHeight="lg">{title}</Text>
         <HStack spacing={4}>
           {/* how to tell it is a single or reoccuring event  */}
-          <Text fontSize='12'><TimeIcon w={4} pr={1}/>See posting details</Text>
+          <Text fontSize='12'><TimeIcon w={4} pr={1}/>See Posting Details
+           </Text>
           <Text fontSize='12'><CalendarIcon w={4} pr={1}/>{`${startDate.toLocaleDateString('en-US', { weekday: 'long', month:'long', day: '2-digit'})} - ${endDate.toLocaleDateString('en-US', { weekday: 'long', month:'long', day: '2-digit'})}`}</Text>
         </HStack>
         <Text lineHeight="1.1rem">{description}</Text>
         <HStack>
           <Text>Skills: </Text>
           {skills.map(skill => (
-            <Badge color="#868686" fontSize='0.8em' fontWeight={400} borderRadius="0.5rem" variant="outline" lineHeight="200%" key={skill.id}>{skill.name}</Badge>
+            <Badge color="gray" fontSize='0.8em' fontWeight={400} borderRadius="0.5rem" variant="outline" lineHeight="200%" key={skill.id}>{skill.name}</Badge>
           ))}
         </HStack>
         <Divider mb={1}/>
         <HStack justifyContent="space-between" w='100%'>
-          <Text color='gray.500' fontSize='12'>Deadline: {autoClosingDate.toLocaleDateString('en-US', { weekday: 'long', month:'long', day: '2-digit'})}</Text>
+          <Text color='gray' fontSize='12'>Deadline: {autoClosingDate.toLocaleDateString('en-US', { weekday: 'long', month:'long', day: '2-digit'})}</Text>
           <ButtonGroup spacing='4' size="xs">
             <Button variant="ghost" color="violet" borderColor="violet">View Details</Button>
             <Button variant="solid" bg="violet" color="white" >Submit Availability</Button>
