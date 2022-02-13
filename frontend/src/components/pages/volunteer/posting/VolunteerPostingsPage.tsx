cd5b32a2997dc3227fa001ae217146fdf43d09bf
Boya <boyazhang123@gmail.com>
2022-02-13 14:57:10 -0800

    complete volunteer postings page frontend + data fetching

--- frontend/src/components/pages/volunteer/posting/VolunteerPostingsPage.tsx
+++ frontend/src/components/pages/volunteer/posting/VolunteerPostingsPage.tsx
@@ -25,56 +25,70 @@ const postingArr = [
     description:
       "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis.",
   },
-  // {
-  //   id: "2",
-  //   skills: [
-  //     { id: "1", name: "CPR" },
-  //     { id: "2", name: "WHMIS" },
-  //     { id: "3", name: "Photocopying" },
-  //   ],
-  //   branch: {
-  //     id: "1",
-  //     name: "sample text",
-  //   },
-  //   title: "Medical Reception Volunteer",
-  //   startDate: new Date("December 17, 2021"),
-  //   endDate: new Date("January 20, 2022"),
-  //   autoClosingDate: new Date("Monday, November 30"),
-  //   isSignedUp: false,
-  //   description:
-  //     "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis.",
-  // },
-  // {
-  //   id: "3",
-  //   skills: [
-  //     { id: "1", name: "CPR" },
-  //     { id: "2", name: "WHMIS" },
-  //     { id: "3", name: "Photocopying" },
-  //   ],
-  //   branch: {
-  //     id: "1",
-  //     name: "sample text",
-  //   },
-  //   title: "Medical Reception Volunteer",
-  //   startDate: new Date("December 17, 2021"),
-  //   endDate: new Date("January 20, 2022"),
-  //   autoClosingDate: new Date("Monday, November 30"),
-  //   isSignedUp: false,
-  //   description:
-  //     "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis.",
-  // },
+  {
+    id: "2",
+    skills: [
+      { id: "1", name: "CPR" },
+      { id: "2", name: "WHMIS" },
+      { id: "3", name: "Photocopying" },
+    ],
+    branch: {
+      id: "1",
+      name: "sample text",
+    },
+    title: "Medical Reception Volunteer",
+    startDate: new Date("December 17, 2021"),
+    endDate: new Date("January 20, 2022"),
+    autoClosingDate: new Date("Monday, November 30"),
+    isSignedUp: false,
+    description:
+      "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis.",
+  },
+  {
+    id: "3",
+    skills: [
+      { id: "1", name: "CPR" },
+      { id: "2", name: "WHMIS" },
+      { id: "3", name: "Photocopying" },
+    ],
+    branch: {
+      id: "1",
+      name: "sample text",
+    },
+    title: "Medical Reception Volunteer",
+    startDate: new Date("December 17, 2021"),
+    endDate: new Date("January 20, 2022"),
+    autoClosingDate: new Date("Monday, November 30"),
+    isSignedUp: false,
+    description:
+      "Volunteers will be responsible for updating the inventory monthly, updating the manuals and guidelines on an ongoing basis, uploading the COVID-19 screenings on a weekly basis.",
+  },
 ];
 
+
+// can refactor this query in the future to be more specific (depending on the data this page needs)
 const POSTINGS = gql`
   query VolunteerPostingsPage_postings {
     postings {
       id
+      branch {
+        id
+        name
+      }
+      shifts {
+        id
+        postingId
+        startTime
+        endTime
+      }
       skills {
         id
         name
       }
       employees {
+        id
         userId
+        branchId
       }
       title
       type
@@ -90,15 +104,18 @@ const POSTINGS = gql`
 
 const VolunteerPostingsPage = (): React.ReactElement => {
   const [postings, setPostings] = useState<PostingResponseDTO[] | null>(null);
-
+  console.log(postings)
   useQuery(POSTINGS, {
     fetchPolicy: "cache-and-network",
     onCompleted: (data) => setPostings(data.postings),
   });
 
+
+  // need to refactor this function based on definition of what is an opportunity / event
   const isVolunteerOpportunity = (start: Date, end: Date): boolean => {
     return end.getTime() - start.getTime() > 1000 * 60 * 60 * 24;
   };
+
   const volunteerOpportunities = postingArr?.filter((posting) =>
     isVolunteerOpportunity(posting.startDate, posting.endDate),
   );
@@ -106,24 +123,23 @@ const VolunteerPostingsPage = (): React.ReactElement => {
     (posting) => !isVolunteerOpportunity(posting.startDate, posting.endDate),
   );
 
-  // const {loading, error, postings} = useQuery(GET_POSTINGS)
   return (
-    <Box bg="#D8AEFF" pt={8} minHeight="100vh">
+    <Box bg="background.light" pt="48px" minHeight="100vh">
       <Box maxW="1280px" mx="auto">
-        <HStack mt={8} mb={6} mx={1} justify="space-between" bg="#F4F4F4">
-          <Text fontSize="xl" fontWeight="600">
+        <HStack justify="space-between" bg="#F4F4F4" pb="36px">
+          <Text textStyle="display-small-semibold">
             Events
           </Text>
           <HStack>
-            <Text fontSize="sm" fontWeight="400">
+            <Text>
               Showing:{" "}
             </Text>
             <Select
+            width="194px"
               placeholder="This week"
-              size="xs"
+              size="sm"
               bg="white"
-              borderColor="white"
-              focusBorderColor="white"
+              borderRadius="4px"
             >
               <option value="option2">Next week</option>
               <option value="option3">This month</option>
@@ -132,7 +148,7 @@ const VolunteerPostingsPage = (): React.ReactElement => {
           </HStack>
         </HStack>
         {events?.map((posting) => (
-          <>
+          <Box key={posting.id} pt="36px">
             <PostingCard
               key={posting.id}
               id={posting.id}
@@ -145,25 +161,26 @@ const VolunteerPostingsPage = (): React.ReactElement => {
               branchName={posting.branch.name}
               type="EVENT"
             />
-            <div style={{ padding: "2rem" }} />
-          </>
+          </Box>
         ))}
-        <Text fontSize="xl" fontWeight="600">
+        <Text textStyle="display-small-semibold" pb="36px">
           Volunteer Opportunities
         </Text>
         {volunteerOpportunities?.map((posting) => (
-          <PostingCard
-            key={posting.id}
-            id={posting.id}
-            skills={posting.skills}
-            title={posting.title}
-            startDate={posting.startDate}
-            endDate={posting.endDate}
-            autoClosingDate={posting.autoClosingDate}
-            description={posting.description}
-            branchName={posting.branch.name}
-            type="OPPORTUNITY"
-          />
+           <Box key={posting.id} pb="36px">
+              <PostingCard
+                key={posting.id}
+                id={posting.id}
+                skills={posting.skills}
+                title={posting.title}
+                startDate={posting.startDate}
+                endDate={posting.endDate}
+                autoClosingDate={posting.autoClosingDate}
+                description={posting.description}
+                branchName={posting.branch.name}
+                type="OPPORTUNITY"
+              />
+          </Box>
         ))}
       </Box>
     </Box>
