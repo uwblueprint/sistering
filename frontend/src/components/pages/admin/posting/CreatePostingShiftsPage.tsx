c72bd40323ca50db90df1ead5af8f635b9b82e24
Lambert Liu <lambert0928@outlook.com>
2022-02-17 20:09:01 -0500

    Move calendar to create posting shifts page

--- frontend/src/components/pages/admin/posting/CreatePostingShiftsPage.tsx
+++ frontend/src/components/pages/admin/posting/CreatePostingShiftsPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Container, Divider } from "@chakra-ui/react";
+import ShiftCalendar from "../../../admin/ShiftCalendar/ShiftCalendar";
 
 const CreatePostingShiftsPage = (): React.ReactElement => {
-  return <div />;
+  return (
+    <Container maxW="container.lg">
+      <ShiftCalendar />
+      <Divider my={4} />
+    </Container>
+  );
 };
 
 export default CreatePostingShiftsPage;
