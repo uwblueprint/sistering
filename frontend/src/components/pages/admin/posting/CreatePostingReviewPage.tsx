456a0c86c53d931f189a81e55acf62ef82bdfae1
Brian Tu <brianptu2@gmail.com>
2022-02-17 21:41:40 -0500

    Add API call functionality to create posting (#147)
    * Add API call functionality to create posting
    
    Fix button name
    
    * Implement requested changes

--- frontend/src/components/pages/admin/posting/CreatePostingReviewPage.tsx
+++ frontend/src/components/pages/admin/posting/CreatePostingReviewPage.tsx
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Button, useBoolean } from "@chakra-ui/react";
+import { gql, useMutation } from "@apollo/client";
 
 import CreatePostingReview from "../../../admin/posting/CreatePostingReview";
 import MainPageButton from "../../../common/MainPageButton";
 
+import PostingContext from "../../../../contexts/admin/PostingContext";
+
+const CREATE_POSTING = gql`
+  mutation CreatePosting($posting: PostingRequestDTO!) {
+    createPosting(posting: $posting) {
+      id
+      title
+    }
+  }
+`;
+
 const CreatePostingReviewPage = (): React.ReactElement => {
+  const { status, times, recurrenceInterval, ...postingToCreate } = useContext(
+    PostingContext,
+  );
+  const [createPosting, { loading, error }] = useMutation(CREATE_POSTING);
+  const [isDraftClicked, setIsDraftClicked] = useBoolean();
+
+  // eslint-disable-next-line no-alert
+  if (error) window.alert(error);
+
   return (
     <div>
       <CreatePostingReview />
       <MainPageButton />
+      <Button
+        variant="outline"
+        isLoading={loading && isDraftClicked}
+        onClick={() => {
+          setIsDraftClicked.on();
+          createPosting({
+            variables: {
+              posting: { ...postingToCreate, status: "DRAFT" },
+            },
+          });
+        }}
+      >
+        Save as Draft
+      </Button>
+      <Button
+        isLoading={loading && !isDraftClicked}
+        onClick={() => {
+          setIsDraftClicked.off();
+          createPosting({
+            variables: {
+              posting: { ...postingToCreate, status: "PUBLISHED" },
+            },
+          });
+        }}
+      >
+        Post
+      </Button>
     </div>
   );
 };
