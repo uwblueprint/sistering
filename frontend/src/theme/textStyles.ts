fdbb17c90cb4f81f5980aae4fb5603b4d1b339d3
Brian Tu <brianptu2@gmail.com>
2022-02-20 16:57:16 -0500

    Create textStyles.ts and fix tag baseStyle (#165)

---
+++ frontend/src/theme/textStyles.ts
@@ -0,0 +1,82 @@
+const textStyles = {
+  "display-large": {
+    fontFamily: "Raleway",
+    fontSize: "28px",
+    fontWeight: "semibold",
+    lineHeight: "140%",
+    color: "text.default",
+  },
+  "display-medium": {
+    fontFamily: "Raleway",
+    fontSize: "26px",
+    fontWeight: "semibold",
+    lineHeight: "140%",
+    color: "text.default",
+  },
+  "display-small-semibold": {
+    fontFamily: "Raleway",
+    fontSize: "24px",
+    fontWeight: "semibold",
+    lineHeight: "150%",
+    color: "text.default",
+  },
+  "display-small-regular": {
+    fontFamily: "Raleway",
+    fontSize: "24px",
+    fontWeight: "normal",
+    lineHeight: "150%",
+    color: "text.default",
+  },
+  heading: {
+    fontFamily: "Raleway",
+    fontSize: "20px",
+    fontWeight: "semibold",
+    lineHeight: "150%",
+    color: "text.default",
+  },
+  subheading: {
+    fontFamily: "Raleway",
+    fontSize: "16px",
+    fontWeight: "semibold",
+    lineHeight: "130%",
+    textTransform: "uppercase",
+    color: "text.default",
+  },
+  "button-semibold": {
+    fontFamily: "Raleway",
+    fontSize: "18px",
+    fontWeight: "semibold",
+    lineHeight: "24px",
+    color: "text.default",
+  },
+  "button-regular": {
+    fontFamily: "Raleway",
+    fontSize: "18px",
+    fontWeight: "normal",
+    lineHeight: "24px",
+    color: "text.default",
+  },
+  "body-regular": {
+    fontFamily: "Inter",
+    fontSize: "18px",
+    fontWeight: "normal",
+    lineHeight: "150%",
+    color: "text.default",
+  },
+  "body-bold": {
+    fontFamily: "Inter",
+    fontSize: "18px",
+    fontWeight: "bold",
+    lineHeight: "150%",
+    color: "text.default",
+  },
+  caption: {
+    fontFamily: "Inter",
+    fontSize: "16px",
+    fontWeight: "normal",
+    lineHeight: "150%",
+    color: "text.default",
+  },
+};
+
+export default textStyles;
