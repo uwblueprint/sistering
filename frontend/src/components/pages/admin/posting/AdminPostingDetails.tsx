a9c2b34d51a44f97773fc4e5e4f45baef6dbd765
albertlai431 <albertlai631@outlook.com>
2022-02-23 09:05:45 -0500

    Run lint

--- frontend/src/components/pages/admin/posting/AdminPostingDetails.tsx
+++ frontend/src/components/pages/admin/posting/AdminPostingDetails.tsx
@@ -1,20 +1,10 @@
 import React, { useState } from "react";
-import {
-    Text,
-    VStack,
-    HStack,
-    Box,
-    Container,
-    Divider,
-    Button,
-    Flex,
-} from "@chakra-ui/react";
+import { VStack, Box, Container, Button, Flex } from "@chakra-ui/react";
 
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { PostingResponseDTO } from "../../../../types/api/PostingTypes";
-import { formatDateString } from "../../../../utils/DateUtils";
 import PostingDetails from "../../../common/PostingDetails";
 
 const POSTING = gql`
@@ -40,44 +30,44 @@ const POSTING = gql`
 `;
 
 const AdminPostingDetails = (): React.ReactElement => {
-    const { id } = useParams<{ id: string }>();
-    const [
-        postingDetails,
-        setPostingDetails,
-    ] = useState<PostingResponseDTO | null>(null);
-    useQuery(POSTING, {
-        variables: { id },
-        fetchPolicy: "cache-and-network",
-        onCompleted: (data) => {
-            setPostingDetails(data.posting);
-        },
-    });
-    return (
-        <Box bg="background.light" py={7} px={10} minH="100vh">
-            <VStack>
-                <Container pt={3} pb={6} px={0} maxW="container.xl">
-                    <Flex justifyContent="flex-start">
-                        <Button leftIcon={<ChevronLeftIcon />} variant="link">
-                            Back to editing
-                        </Button>
-                    </Flex>
-                </Container>
+  const { id } = useParams<{ id: string }>();
+  const [
+    postingDetails,
+    setPostingDetails,
+  ] = useState<PostingResponseDTO | null>(null);
+  useQuery(POSTING, {
+    variables: { id },
+    fetchPolicy: "cache-and-network",
+    onCompleted: (data) => {
+      setPostingDetails(data.posting);
+    },
+  });
+  return (
+    <Box bg="background.light" py={7} px={10} minH="100vh">
+      <VStack>
+        <Container pt={3} pb={6} px={0} maxW="container.xl">
+          <Flex justifyContent="flex-start">
+            <Button leftIcon={<ChevronLeftIcon />} variant="link">
+              Back to editing
+            </Button>
+          </Flex>
+        </Container>
 
-                <Container
-                    bg="background.white"
-                    maxW="container.xl"
-                    centerContent
-                    borderRadius={10}
-                >
-                    {postingDetails ? (
-                        <Box p={6} w="full">
-                            <PostingDetails postingDetails={postingDetails} />
-                        </Box>
-                    ) : null}
-                </Container>
-            </VStack>
-        </Box>
-    );
+        <Container
+          bg="background.white"
+          maxW="container.xl"
+          centerContent
+          borderRadius={10}
+        >
+          {postingDetails ? (
+            <Box p={6} w="full">
+              <PostingDetails postingDetails={postingDetails} />
+            </Box>
+          ) : null}
+        </Container>
+      </VStack>
+    </Box>
+  );
 };
 
 export default AdminPostingDetails;
