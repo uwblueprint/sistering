851f20b519c37cace0aba45213cfbe4d74af7043
briantu <brianptu2@gmail.com>
2022-03-03 20:44:08 -0500

    Use react router link and volunteer pages enum

--- frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
+++ frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
 import VolunteerNavbar from "../../../volunteer/VolunteerNavbar";
+import { VolunteerPages } from "../../../../constants/Volunteer";
 
 const VolunteerShiftsPage = (): React.ReactElement => {
   return (
     <div>
-      <VolunteerNavbar defaultIndex={0} />
+      <VolunteerNavbar defaultIndex={VolunteerPages.VolunteerShiftsPage} />
       <Box bg="background.light">
         <Text textStyle="display-large">Volunteer Shifts</Text>
       </Box>
--- frontend/src/components/volunteer/VolunteerNavbar.tsx
+++ frontend/src/components/volunteer/VolunteerNavbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   Flex,
   Box,
@@ -13,59 +13,54 @@ import {
   MenuItem,
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-
+import { VolunteerPages } from "../../constants/Volunteer";
 import Sistering_Screen_RGB from "../../assets/Sistering_Screen_RGB.png";
 
 const VolunteerNavbar = ({
   defaultIndex,
 }: {
-  defaultIndex: number;
+  defaultIndex: VolunteerPages;
 }): React.ReactElement => {
   return (
     <Box px="110px">
       <Flex h="80px" alignItems="center" justifyContent="space-between">
         <Image src={Sistering_Screen_RGB} alt="Sistering logo" h={14} />
-        <Route
-          render={({ history }) => (
-            <Tabs
-              defaultIndex={defaultIndex}
-              alignSelf="flex-end"
-              onChange={(index) => {
-                if (index === 0) history.push("/volunteer/shifts");
-                else history.push("/volunteer/postings");
-              }}
-            >
-              <TabList>
-                <Tab
-                  color="violet"
-                  _hover={{
-                    borderColor: "currentColor",
-                  }}
-                  _selected={{
-                    color: "teal",
-                    borderColor: "currentColor",
-                  }}
-                  py="26px"
-                >
-                  My Shifts
-                </Tab>
-                <Tab
-                  color="violet"
-                  _hover={{
-                    borderColor: "currentColor",
-                  }}
-                  _selected={{
-                    color: "teal",
-                    borderColor: "currentColor",
-                  }}
-                  py="26px"
-                >
-                  Browse Volunteer Postings
-                </Tab>
-              </TabList>
-            </Tabs>
-          )}
-        />
+        <Tabs defaultIndex={defaultIndex} alignSelf="flex-end">
+          <TabList>
+            <Link to="/volunteer/shifts" style={{ textDecoration: "none" }}>
+              <Tab
+                color="violet"
+                _hover={{
+                  borderColor: "currentColor",
+                  textDecoration: "none",
+                }}
+                _selected={{
+                  color: "teal",
+                  borderColor: "currentColor",
+                  textDecoration: "none",
+                }}
+                py="26px"
+              >
+                My Shifts
+              </Tab>
+            </Link>
+            <Link to="/volunteer/postings" style={{ textDecoration: "none" }}>
+              <Tab
+                color="violet"
+                _hover={{
+                  borderColor: "currentColor",
+                }}
+                _selected={{
+                  color: "teal",
+                  borderColor: "currentColor",
+                }}
+                py="26px"
+              >
+                Browse Volunteer Postings
+              </Tab>
+            </Link>
+          </TabList>
+        </Tabs>
         <Menu>
           <MenuButton
             textStyle="button-semibold"
