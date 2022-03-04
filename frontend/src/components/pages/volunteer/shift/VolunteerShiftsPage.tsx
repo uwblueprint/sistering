fef987f330a1c073025f247741d3d2e130d651f8
briantu <brianptu2@gmail.com>
2022-03-03 20:44:08 -0500

    Use react router link and volunteer pages enum

--- frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
+++ frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
 import VolunteerNavbar from "../../../volunteer/VolunteerNavbar";
-<<<<<<< HEAD
 import { VolunteerPages } from "../../../../constants/Volunteer";
-=======
->>>>>>> Create VolunteerNavbar
 
 const VolunteerShiftsPage = (): React.ReactElement => {
   return (
     <div>
-<<<<<<< HEAD
       <VolunteerNavbar defaultIndex={VolunteerPages.VolunteerShiftsPage} />
-=======
-      <VolunteerNavbar defaultIndex={0} />
->>>>>>> Create VolunteerNavbar
       <Box bg="background.light">
         <Text textStyle="display-large">Volunteer Shifts</Text>
       </Box>
