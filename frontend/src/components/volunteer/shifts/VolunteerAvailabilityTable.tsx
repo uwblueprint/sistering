2012427c0737f06c9f38a40c3d8db1b9b468594a
Rickson Yang <rixin.yang.2001@gmail.com>
2022-03-06 16:23:41 -0500

    temp headers implementation

--- frontend/src/components/volunteer/shifts/VolunteerAvailabilityTable.tsx
+++ frontend/src/components/volunteer/shifts/VolunteerAvailabilityTable.tsx
@@ -1,4 +1,13 @@
-import { Table, Tbody, Td, Text, Tr } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Select,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Tr,
+} from "@chakra-ui/react";
 import React from "react";
 import moment from "moment";
 import NoShiftsAvailableTableRow from "./NoShiftsAvailableTableRow";
@@ -18,11 +27,23 @@ const VolunteerAvailabilityTable = ({
   // Side note: I think we should try to avoid doing queries in table components when avbailable03
   // Query to get some posting
 
+  // We assume that we have some posting
+  // we generate an item in the the headers menu for each week between start and end state
+
   // Sample data
-  const current = new Date();
-  const postingStartWeek = new Date(
-    current.setDate(current.getDate() - current.getDay()),
+  const postingStartDate = new Date(
+    new Date().setDate(new Date().getDate() - 14),
   );
+
+  const postingEndDate = new Date(
+    new Date().setDate(new Date().getDate() + 14),
+  );
+
+  // Maybe put under use effect
+  const postingStartWeek = new Date(
+    new Date().setDate(postingStartDate.getDate() - postingStartDate.getDay()),
+  );
+
   const postingShifts: Shift[] = [
     {
       startTime: new Date(),
@@ -45,63 +66,156 @@ const VolunteerAvailabilityTable = ({
       ),
     },
   ];
+  // End of sample data
 
   const [currentWeek, setWeek] = React.useState(postingStartWeek);
   const [shifts, setShifts] = React.useState(postingShifts);
 
-  return (
-    <Table variant="striped" colorScheme="teal" w="100%">
-      {Array.from(Array(7).keys()).map((day) => {
-        const date = new Date(
-          new Date(currentWeek).setDate(currentWeek.getDate() + day),
-        );
+  console.log(postingStartDate);
+  console.log(postingEndDate);
+  console.log(
+    Math.ceil(
+      Math.abs(postingEndDate.getTime() - postingStartWeek.getTime()) /
+        (1000 * 3600 * 24),
+    ),
+  );
 
-        return (
-          <Tbody key={day}>
-            <Tr>
-              <Td bgColor="background.light">
-                <Text textStyle="body-bold">
-                  {date.toLocaleDateString("en-US", {
-                    weekday: "long",
+  return (
+    <>
+      <Flex>
+        <Button
+          disabled={currentWeek === postingStartWeek}
+          onClick={() =>
+            setWeek(new Date(new Date().setDate(currentWeek.getDate() - 7)))
+          }
+        >
+          Previous week
+        </Button>
+        <Select
+          defaultValue={0}
+          onChange={(e) =>
+            setWeek(
+              new Date(
+                new Date().setDate(
+                  postingStartWeek.getDate() -
+                    postingStartWeek.getDay() +
+                    7 * Number(e.target.value),
+                ),
+              ),
+            )
+          }
+        >
+          {Array(
+            Math.ceil(
+              Math.ceil(
+                Math.abs(
+                  postingEndDate.getTime() - postingStartWeek.getTime(),
+                ) /
+                  (1000 * 3600 * 24),
+              ) / 7,
+            ),
+          )
+            .fill(0)
+            .map((_, i) => i)
+            .map((week) => {
+              const startWeek = new Date(
+                new Date().setDate(
+                  postingStartDate.getDate() -
+                    postingStartDate.getDay() +
+                    7 * week,
+                ),
+              );
+              return (
+                <option key={week} value={week}>
+                  {startWeek.toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}{" "}
+                  -{" "}
+                  {new Date(
+                    new Date(startWeek).setDate(startWeek.getDate() + 6),
+                  ).toLocaleDateString("en-US", {
                     year: "numeric",
                     month: "long",
                     day: "numeric",
                   })}
-                </Text>
-              </Td>
-            </Tr>
-            {shifts.filter(
-              (shift) =>
-                moment(shift.startTime).diff(date, "days", false) === 0,
-            ).length < 1 ? (
+                </option>
+              );
+            })}
+        </Select>
+
+        <Button
+          disabled={
+            new Date(
+              new Date().setDate(currentWeek.getDate() - currentWeek.getDay()),
+            ) ===
+            new Date(
+              new Date().setDate(
+                postingEndDate.getDate() - postingEndDate.getDay(),
+              ),
+            )
+          }
+          onClick={() =>
+            setWeek(new Date(new Date().setDate(currentWeek.getDate() + 7)))
+          }
+        >
+          Next week
+        </Button>
+      </Flex>
+      <Table variant="striped" colorScheme="teal" w="100%">
+        {Array.from(Array(7).keys()).map((day) => {
+          const date = new Date(
+            new Date(currentWeek).setDate(currentWeek.getDate() + day),
+          );
+
+          return (
+            <Tbody key={day}>
               <Tr>
-                <Td>
-                  <NoShiftsAvailableTableRow />
+                <Td bgColor="background.light">
+                  <Text textStyle="body-bold">
+                    {date.toLocaleDateString("en-US", {
+                      weekday: "long",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </Text>
                 </Td>
               </Tr>
-            ) : (
-              shifts
-                .filter(
-                  (shift) =>
-                    moment(shift.startTime).diff(date, "days", false) === 0,
-                )
-                .map((shift, index) => {
-                  return (
-                    <Tr key={`${day}-${index}`}>
-                      <Td p={0}>
-                        <VolunteerAvailabilityTableRow
-                          start={shift.startTime}
-                          end={shift.endTime}
-                        />
-                      </Td>
-                    </Tr>
-                  );
-                })
-            )}
-          </Tbody>
-        );
-      })}
-    </Table>
+              {shifts.filter(
+                (shift) =>
+                  moment(shift.startTime).diff(date, "days", false) === 0,
+              ).length < 1 ? (
+                <Tr>
+                  <Td>
+                    <NoShiftsAvailableTableRow />
+                  </Td>
+                </Tr>
+              ) : (
+                shifts
+                  .filter(
+                    (shift) =>
+                      moment(shift.startTime).diff(date, "days", false) === 0,
+                  )
+                  .map((shift, index) => {
+                    return (
+                      <Tr key={`${day}-${index}`}>
+                        <Td p={0}>
+                          <VolunteerAvailabilityTableRow
+                            start={shift.startTime}
+                            end={shift.endTime}
+                          />
+                        </Td>
+                      </Tr>
+                    );
+                  })
+              )}
+            </Tbody>
+          );
+        })}
+      </Table>
+    </>
   );
 };
 
