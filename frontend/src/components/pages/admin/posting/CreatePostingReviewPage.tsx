86f48b39fd58b10489e934a04eebf09fc0983e54
Sherry Li <sherryhli@users.noreply.github.com>
2022-03-08 13:54:23 -0500

    Implement hi-fi layouts for admin posting pages

--- frontend/src/components/pages/admin/posting/CreatePostingReviewPage.tsx
+++ frontend/src/components/pages/admin/posting/CreatePostingReviewPage.tsx
@@ -4,12 +4,14 @@ import {
   Button,
   Container,
   HStack,
+  Text,
   useBoolean,
   VStack,
 } from "@chakra-ui/react";
 import { gql, useMutation } from "@apollo/client";
 
 import CreatePostingReview from "../../../admin/posting/CreatePostingReview";
+import SideNavBar from "../../../common/SideNavbar";
 import { HOME_PAGE } from "../../../../constants/Routes";
 import PostingContext from "../../../../contexts/admin/PostingContext";
 
@@ -23,11 +25,22 @@ const CREATE_POSTING = gql`
 `;
 
 const CreatePostingReviewPage = (): React.ReactElement => {
-  const { status, times, recurrenceInterval, ...postingToCreate } = useContext(
-    PostingContext,
-  );
+  const {
+    status,
+    times,
+    recurrenceInterval,
+    ...postingToCreateFromContext
+  } = useContext(PostingContext);
   const [createPosting, { loading, error }] = useMutation(CREATE_POSTING);
   const [isDraftClicked, setIsDraftClicked] = useBoolean();
+
+  const { branch, skills, employees, ...rest } = postingToCreateFromContext;
+  const postingToCreate = {
+    ...rest,
+    branchId: branch.id,
+    skills: skills.map((skill) => skill.id),
+    employees: employees.map((employee) => employee.id),
+  };
 
   const history = useHistory();
   const navigateToHome = () => history.push(HOME_PAGE);
@@ -36,10 +49,19 @@ const CreatePostingReviewPage = (): React.ReactElement => {
   if (error) window.alert(error);
 
   return (
-    <div>
-      <CreatePostingReview />
-      <Container maxW="container.xl" p={0}>
+    <Container maxW="container.xl" p={0}>
+      <HStack alignItems="flex-start" spacing={0}>
+        <VStack alignItems="flex-start" spacing="90px">
+          <Text textStyle="display-medium" pt={10}>
+            Create New Posting
+          </Text>
+          <SideNavBar
+            activeStep={2}
+            labels={["Basic Information", "Time Slots", "Review and Post"]}
+          />
+        </VStack>
         <VStack alignItems="flex-end">
+          <CreatePostingReview />
           <HStack spacing="16px">
             <Button
               variant="outline"
@@ -72,8 +94,8 @@ const CreatePostingReviewPage = (): React.ReactElement => {
             </Button>
           </HStack>
         </VStack>
-      </Container>
-    </div>
+      </HStack>
+    </Container>
   );
 };
 
