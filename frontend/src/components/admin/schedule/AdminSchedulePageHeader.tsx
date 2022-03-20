eaa1ec62d2727be0504121b1902509b5506169e7
briantu <brianptu2@gmail.com>
2022-03-20 16:34:31 -0400

    Create header component

---
+++ frontend/src/components/admin/schedule/AdminSchedulePageHeader.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Flex, Box, Tag, Button } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+
+const AdminSchedulePageHeader = ({
+  branchName,
+}: {
+  branchName: string;
+}): React.ReactElement => {
+  return (
+    <Box px="40px">
+      <Flex h="50px" alignItems="center" justifyContent="space-between">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Button leftIcon={<ChevronLeftIcon w={8} h={8} />} variant="link">
+            Back to homepage
+          </Button>
+        </Link>
+        <Tag mr={3}>{branchName}</Tag>
+      </Flex>
+    </Box>
+  );
+};
+
+export default AdminSchedulePageHeader;
