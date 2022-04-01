74a2031f73d91e8d8c409e812a0e1029df4cd7a5
briantu <brianptu2@gmail.com>
2022-03-31 21:20:43 -0400

    Create no shifts panel

--- frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
+++ frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Flex, Box, Text } from "@chakra-ui/react";
 import VolunteerNavbar from "../../../volunteer/VolunteerNavbar";
 import { VolunteerPages } from "../../../../constants/Volunteer";
+import NoShiftsPanel from "../../admin/schedule/NoShiftsPanel";
 
 const VolunteerShiftsPage = (): React.ReactElement => {
   return (
-    <div>
+    <Flex h="100vh" flexFlow="column">
       <VolunteerNavbar defaultIndex={VolunteerPages.VolunteerShiftsPage} />
       <Box bg="background.light">
         <Text textStyle="display-large">Volunteer Shifts</Text>
       </Box>
-    </div>
+      <Box w="400px" h="full">
+        <NoShiftsPanel />
+      </Box>
+    </Flex>
   );
 };
 
