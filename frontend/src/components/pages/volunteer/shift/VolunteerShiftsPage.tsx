2db8ef9a458ee5d9bb56cf3426e5cac324e39244
briantu <brianptu2@gmail.com>
2022-04-03 16:28:05 -0400

    Remove demo

--- frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
+++ frontend/src/components/pages/volunteer/shift/VolunteerShiftsPage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Text } from "@chakra-ui/react";
 import VolunteerNavbar from "../../../volunteer/VolunteerNavbar";
 import { VolunteerPages } from "../../../../constants/Volunteer";
-import ShiftTimeHeader from "../../../admin/schedule/ShiftTimeHeader";
 
 const VolunteerShiftsPage = (): React.ReactElement => {
   return (
@@ -11,21 +10,6 @@ const VolunteerShiftsPage = (): React.ReactElement => {
       <Box bg="background.light">
         <Text textStyle="display-large">Volunteer Shifts</Text>
       </Box>
-      <ShiftTimeHeader
-        shifts={[
-          {
-            shiftId: 1,
-            shiftStartTime: new Date("2022-04-11T13:30"),
-            shiftEndTime: new Date("2022-04-11T15:30"),
-          },
-          {
-            shiftId: 2,
-            shiftStartTime: new Date("2022-04-11T16:30"),
-            shiftEndTime: new Date("2022-04-11T18:30"),
-          },
-        ]}
-        onShiftSelected={(id: number) => console.log(id)}
-      />
     </div>
   );
 };
