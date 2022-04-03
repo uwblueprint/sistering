82fb76149ec7f3867dc58bfe0884112c1e8de5ff
Brian Tu <brianptu2@gmail.com>
2022-04-03 16:33:07 -0400

    Edit volunteers shift time header (#251)
    * Create ShiftTimeHeader component
    
    * Change ShiftTimeHeader props
    
    * Remove demo

---
+++ frontend/src/components/admin/schedule/ShiftTimeHeader.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { VStack, Text, Select } from "@chakra-ui/react";
+import { formatTimeHourMinutes } from "../../../utils/DateTimeUtils";
+
+type Shift = {
+  shiftId: number;
+  shiftStartTime: Date;
+  shiftEndTime: Date;
+};
+
+type ShiftTimeHeaderProps = {
+  shifts: Shift[];
+  onShiftSelected: (id: number) => void;
+};
+
+const ShiftTimeHeader: React.FC<ShiftTimeHeaderProps> = ({
+  shifts,
+  onShiftSelected,
+}: ShiftTimeHeaderProps): React.ReactElement => {
+  return (
+    <VStack
+      spacing="12px"
+      px="32px"
+      py="16px"
+      alignItems="flex-start"
+      borderBottom="2px"
+      borderLeft="2px"
+      borderColor="background.dark"
+    >
+      <Text textStyle="body-bold">Shift Time</Text>
+      <Select
+        size="md"
+        textStyle="caption"
+        onChange={(e) => onShiftSelected(Number(e.target.value))}
+      >
+        {shifts.map((shift, i) => (
+          <option key={i} value={shift.shiftId}>
+            {`${formatTimeHourMinutes(
+              shift.shiftStartTime,
+            )} - ${formatTimeHourMinutes(shift.shiftEndTime)}`}
+          </option>
+        ))}
+      </Select>
+    </VStack>
+  );
+};
+
+export default ShiftTimeHeader;
