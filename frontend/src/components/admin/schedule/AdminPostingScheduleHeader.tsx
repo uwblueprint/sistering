bb0b84f66c39a1ab6e92e3bd95491a3444859d89
hujoseph99 <hujoseph99@gmail.com>
2022-04-14 00:58:24 -0400

    create adminPostingScheduleHeader

---
+++ frontend/src/components/admin/schedule/AdminPostingScheduleHeader.tsx
@@ -0,0 +1,43 @@
+import { Button, Divider, Flex, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+type AdminPostingScheduleHeaderProps = {
+  postingName: string;
+  postingID: number;
+};
+
+const AdminPostingScheduleHeader = ({
+  postingName,
+  postingID,
+}: AdminPostingScheduleHeaderProps): React.ReactElement => {
+  const history = useHistory();
+
+  return (
+    <VStack spacing="15px">
+      <Flex
+        dir="horizontal"
+        justifyContent="space-between"
+        alignItems="center"
+        px="47px"
+        width="100%"
+      >
+        <Text textStyle="display-small-semibold">{postingName}</Text>
+        <Flex dir="horizontal">
+          <Button
+            variant="ghost"
+            textStyle="button-semibold"
+            mr="26px"
+            onClick={() => history.push(`/admin/posting/${postingID}`)}
+          >
+            View Posting
+          </Button>
+          <Button textStyle="button-semibold">Review</Button>
+        </Flex>
+      </Flex>
+      <Divider />
+    </VStack>
+  );
+};
+
+export default AdminPostingScheduleHeader;
