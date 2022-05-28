0af0bb49d388e7cc039c71d9410bcb711bc051e8
Lambert Liu <lambert0928@outlook.com>
2022-05-28 18:58:16 -0400

    Add signup navbar

--- frontend/src/components/pages/NewAccountPage.tsx
+++ frontend/src/components/pages/NewAccountPage.tsx
@@ -1,19 +1,23 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Container, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import SignupNavbar from "../common/SignupNavbar";
 import ProfilePhotoForm from "../user/ProfilePhotoForm";
 
 const NewAccountPage = (): React.ReactElement => {
   const [profilePhoto, setProfilePhoto] = useState<string>("");
   return (
-    <Box align="left">
-      <Text mb={2} textStyle="display-large">
-        Account Creation
-      </Text>
-      <ProfilePhotoForm
-        profilePhoto={profilePhoto}
-        setProfilePhoto={setProfilePhoto}
-      />
-    </Box>
+    <>
+      <SignupNavbar />
+      <Container maxW="container.xl" align="left">
+        <Text mb={2} textStyle="display-large">
+          Account Creation
+        </Text>
+        <ProfilePhotoForm
+          profilePhoto={profilePhoto}
+          setProfilePhoto={setProfilePhoto}
+        />
+      </Container>
+    </>
   );
 };
 
