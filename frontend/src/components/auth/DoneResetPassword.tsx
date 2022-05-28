13a35bb139878db8264e7408a1f8425ba7440ac1
Boya <boyazhang123@gmail.com>
2022-05-28 19:46:23 -0400

    create email sent page*, run yarn fix

--- frontend/src/components/auth/DoneResetPassword.tsx
+++ frontend/src/components/auth/DoneResetPassword.tsx
@@ -4,17 +4,24 @@ import { ReactComponent as Logo } from "../../assets/Sistering_Logo.svg";
 
 const DoneResetPassword = (): React.ReactElement => {
   return (
-  <Flex mx="auto" mt="200px" direction="column" align="center" textAlign="center">
-    <Logo />
-    <Text textStyle="display-large" mb='36px'>
-      Email Sent !
-    </Text>
-    <Text textStyle="caption" maxWidth='650px' >
-    If you have provided us with an existing email you should recieve a password reset link from us. Make sure to check your junk email if you cannot find it!
-    </Text>
-  
-</Flex>
-  )
+    <Flex
+      mx="auto"
+      mt="200px"
+      direction="column"
+      align="center"
+      textAlign="center"
+    >
+      <Logo />
+      <Text textStyle="display-large" mb="36px">
+        Email Sent !
+      </Text>
+      <Text textStyle="caption" maxWidth="650px">
+        If you have provided us with an existing email you should recieve a
+        password reset link from us. Make sure to check your junk email if you
+        cannot find it!
+      </Text>
+    </Flex>
+  );
 };
 
 export default DoneResetPassword;
