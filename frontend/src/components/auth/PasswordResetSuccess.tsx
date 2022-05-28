092175f5e5fa1cc13a2002710650268824233dad
unknown <evanmatt.wong@gmail.com>
2022-05-28 16:28:21 -0400

    changd font sizes to match figma, removed  {''}s

--- frontend/src/components/auth/PasswordResetSuccess.tsx
+++ frontend/src/components/auth/PasswordResetSuccess.tsx
@@ -10,12 +10,12 @@ const PasswordResetSuccessPage = (): React.ReactElement => {
     <Box display="flex">
       <Box margin="48px auto" textAlign="center">
         <Logo />
-        <Text fontSize="xl" fontWeight="bold" marginBottom="24px">
+        <Text fontSize="28px" fontWeight="bold" marginBottom="24px">
           Password Change Successful!
-        </Text>{" "}
-        <Text fontSize="l" marginBottom="24px">
+        </Text>
+        <Text fontSize="14px" marginBottom="24px">
           You may now login with your new credentials.
-        </Text>{" "}
+        </Text>
         <Button
           color="#7000DE"
           width="128px"
