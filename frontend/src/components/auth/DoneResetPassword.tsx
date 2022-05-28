f2585b13214b93a11c38dfcefd8ed1116f62125c
Boya <boyazhang123@gmail.com>
2022-05-28 19:34:25 -0400

    create reset password page

--- frontend/src/components/auth/DoneResetPassword.tsx
+++ frontend/src/components/auth/DoneResetPassword.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Text } from "@chakra-ui/react";
+import { Text, Flex } from "@chakra-ui/react";
+import { ReactComponent as Logo } from "../../assets/Sistering_Logo.svg";
 
-const DoneResetPassword = (): React.ReactElement => (
-  <div style={{ textAlign: "center" }}>
-    <Text textStyle="display-large">Reset Password</Text>
-    <p>
-      We have sent you an e-mail. Please contact us if you do not receive it
-      within a few minutes.
-    </p>
-  </div>
-);
+const DoneResetPassword = (): React.ReactElement => {
+  return (
+  <Flex mx="auto" mt="200px" direction="column" align="center" textAlign="center">
+    <Logo />
+    <Text textStyle="display-large" mb='36px'>
+      Email Sent !
+    </Text>
+    <Text textStyle="caption" maxWidth='650px' >
+    If you have provided us with an existing email you should recieve a password reset link from us. Make sure to check your junk email if you cannot find it!
+    </Text>
+  
+</Flex>
+  )
+};
 
 export default DoneResetPassword;
