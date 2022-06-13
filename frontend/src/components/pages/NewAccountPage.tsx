ed15824aa7b491a0d1f396edb670675017a5249b
Adriana Ceric <62163307+AdrianaCeric@users.noreply.github.com>
2022-06-13 19:39:09 -0400

    add mutation call

--- frontend/src/components/pages/NewAccountPage.tsx
+++ frontend/src/components/pages/NewAccountPage.tsx
@@ -1,12 +1,52 @@
 import { Container, Divider, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 import SignupNavbar from "../common/SignupNavbar";
 import AccountForm from "../user/AccountForm";
 import ProfilePhotoForm from "../user/ProfilePhotoForm";
 
+const CREATE_VOLUNTEER_USER = gql`
+  mutation {
+    createVolunteerUser(
+      volunteerUser: {
+        firstName: "Alice"
+        lastName: "Zoo"
+        email: "alice.zoo@gmail.com"
+        password: "Password1"
+        phoneNumber: "5195468902"
+        dateOfBirth: "2003-10-12"
+        hireDate: "2022-10-12"
+        skills: []
+        branches: []
+      }
+    ) {
+      id
+    }
+  }
+`;
+
+const CREATE_EMPLOYEE_USER = gql`
+  mutation {
+    createEmployeeUser(
+      employeeUser: {
+        firstName: "Rickson"
+        lastName: "Cedric"
+        email: "rick8338@gmail.com"
+        phoneNumber: "5195468902"
+        password: "Password1"
+        branchId: 3
+        title: "CEO"
+      }
+    ) {
+      id
+    }
+  }
+`;
+
 const NewAccountPage = (): React.ReactElement => {
   const [profilePhoto, setProfilePhoto] = useState<string>("");
   const [isAdmin] = useState<boolean>(false);
+  const [clickToCreateEmployee] = useMutation(CREATE_EMPLOYEE_USER);
   return (
     <>
       <SignupNavbar />
