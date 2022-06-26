86dfd4731aa2ba2ab5d46e65de2f9221fc99986b
Boya <boyazhang123@gmail.com>
2022-06-25 21:15:26 -0400

    save rebase changes

--- frontend/src/components/auth/DoneResetPassword.tsx
+++ frontend/src/components/auth/DoneResetPassword.tsx
@@ -1,33 +1,20 @@
 import React from "react";
-import { Box, Text, Flex } from "@chakra-ui/react";
-import AuthNavbar from "./AuthNavbar";
-
+import { Text, Flex } from "@chakra-ui/react";
 import { ReactComponent as Logo } from "../../assets/Sistering_Logo.svg";
 
 const DoneResetPassword = (): React.ReactElement => {
   return (
-    <Box>
-      {" "}
-      <AuthNavbar />
-      <Flex
-        mx="auto"
-        mt="200px"
-        direction="column"
-        align="center"
-        textAlign="center"
-      >
-        <Logo />
-        <Text textStyle="display-large" mb="36px">
-          Email Sent !
-        </Text>
-        <Text textStyle="caption" maxWidth="650px">
-          If you have provided us with an existing email you should recieve a
-          password reset link from us. Make sure to check your junk email if you
-          cannot find it!
-        </Text>
-      </Flex>
-    </Box>
-  );
+  <Flex mx="auto" mt="200px" direction="column" align="center" textAlign="center">
+    <Logo />
+    <Text textStyle="display-large" mb='36px'>
+      Email Sent !
+    </Text>
+    <Text textStyle="caption" maxWidth='650px' >
+    If you have provided us with an existing email you should recieve a password reset link from us. Make sure to check your junk email if you cannot find it!
+    </Text>
+  
+</Flex>
+  )
 };
 
 export default DoneResetPassword;
