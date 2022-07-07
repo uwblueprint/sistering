b7640087d695dcbb31504c525fea06f43bb62bcc
Neel Ismail <neelismail01@gmail.com>
2022-07-07 00:11:52 -0400

    Creating user management side panel + branch selection menu (#449)
    * creating side panel + dropdown
    
    * removing drawer footer buttons
    
    * making background of menu white on hover and focus
    
    * representing branch as BranchResponseDTO
    
    * removing unnecessary import
    
    * making separate profile and multi user drawers + making component for branch selector

--- frontend/src/components/pages/admin/user/AdminUserManagementPage.tsx
+++ frontend/src/components/pages/admin/user/AdminUserManagementPage.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, useDisclosure } from "@chakra-ui/react";
 import { gql, useQuery } from "@apollo/client";
 import { VolunteerUserResponseDTO } from "../../../../types/api/UserType";
 import { EmployeeUserResponseDTO } from "../../../../types/api/EmployeeTypes";
 import ErrorModal from "../../../common/ErrorModal";
 import Loading from "../../../common/Loading";
+import ProfileDrawer from "./ProfileDrawer";
+import { BranchResponseDTO } from "../../../../types/api/BranchTypes";
 
 const USERS = gql`
   query AdminUserManagementPage_Users {
@@ -43,6 +45,20 @@ const AdminUserManagementPage = (): React.ReactElement => {
     EmployeeUserResponseDTO[] | null
   >(null);
 
+  const [branches, setBranches] = useState<BranchResponseDTO[]>([
+    { id: "1", name: "branch1" },
+    { id: "2", name: "branch1" },
+    { id: "3", name: "branch3" },
+    { id: "4", name: "branch4" },
+    { id: "5", name: "branch5" },
+  ]);
+
+  const [selectedBranches, setSelectedBranches] = useState<BranchResponseDTO[]>(
+    [],
+  );
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const { loading, error } = useQuery(USERS, {
     fetchPolicy: "cache-and-network",
     onCompleted: (data) => {
@@ -51,8 +67,26 @@ const AdminUserManagementPage = (): React.ReactElement => {
     },
   });
 
+  const handleBranchMenuItemClicked = (clickedBranch: BranchResponseDTO) => {
+    if (selectedBranches.includes(clickedBranch)) {
+      setSelectedBranches(
+        selectedBranches.filter((branch) => branch !== clickedBranch),
+      );
+    } else {
+      setSelectedBranches([...selectedBranches, clickedBranch]);
+    }
+  };
+
   return (
     <Box>
+      <Button onClick={onOpen}>Open</Button>
+      <ProfileDrawer
+        isOpen={isOpen}
+        branches={branches}
+        selectedBranches={selectedBranches}
+        onClose={onClose}
+        handleBranchMenuItemClicked={handleBranchMenuItemClicked}
+      />
       {loading && <Loading />}
       {error && <ErrorModal />}
     </Box>
