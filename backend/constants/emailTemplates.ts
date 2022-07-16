9e90111c4f0d12a7d77ccb34973405f4611cb9a0
Rickson Yang <44826218+MatoPlus@users.noreply.github.com>
2022-07-16 13:40:33 -0400

    Add email template utils (#490)
    * Add email templates
    
    * Extract out css into constants file

---
+++ backend/constants/emailTemplates.ts
@@ -0,0 +1,239 @@
+export const FONT_LINKS = `
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Raleway:wght@600&family=Work+Sans:wght@700&display=swap"
+    rel="stylesheet"
+  />
+`;
+
+export const ACCOUNT_CREATION_INVITE_STYLE = `
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+    body {
+      font-size: 14px;
+    }
+    .email {
+      background: rgba(255, 255, 255, 1);
+      opacity: 1;
+      overflow: hidden;
+    }
+    .logo {
+      width: 150px;
+      height: 27px;
+      background: url("https://firebasestorage.googleapis.com/v0/b/sistering-dev.appspot.com/o/sistering-logo.png?alt=media&token=58c68ee3-848b-4fcc-902c-77f3dbf252b6");
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: cover;
+      opacity: 1;
+      margin-top: 57px;
+      margin-left: 70px;
+      overflow: hidden;
+    }
+    .title {
+      color: rgba(112, 0, 222, 1);
+      padding-left: 70px;
+      padding-top: 50px;
+      font-family: work sans;
+      font-weight: 700;
+      font-size: 24px;
+      opacity: 1;
+      text-align: left;
+    }
+    .text {
+      width: 600px;
+      color: rgba(0, 0, 0, 1);
+      padding-left: 70px;
+      padding-top: 37px;
+      font-family: inter;
+      font-weight: medium;
+      font-size: 18px;
+      opacity: 1;
+      text-align: left;
+    }
+    .btn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 0.5em 1em;
+      background: #7000de;
+      border-radius: 6px;
+      width: 13em;
+      height: 3em;
+      flex: none;
+      margin-left: 70px;
+      margin-top: 37px;
+      appearance: button;
+      text-decoration: none;
+      color: initial;
+    }
+    .btn-text {
+      font-family: raleway;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 1.3em;
+      display: flex;
+      align-items: center;
+      color: #ffffff;
+      flex: none;
+      order: 1;
+      flex-grow: 0;
+    }
+  </style>
+`;
+
+export const PASSWORD_RESET_STYLE = `
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+    body {
+      font-size: 14px;
+    }
+    .email {
+      background: rgba(255, 255, 255, 1);
+      opacity: 1;
+      overflow: hidden;
+    }
+    .logo {
+      width: 150px;
+      height: 27px;
+      background: url("https://firebasestorage.googleapis.com/v0/b/sistering-dev.appspot.com/o/sistering-logo.png?alt=media&token=58c68ee3-848b-4fcc-902c-77f3dbf252b6");
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: cover;
+      opacity: 1;
+      margin-top: 57px;
+      margin-left: 70px;
+      overflow: hidden;
+    }
+    .title {
+      color: rgba(112, 0, 222, 1);
+      padding-left: 70px;
+      padding-top: 45px;
+      font-family: work sans;
+      font-weight: 700;
+      font-size: 24px;
+      opacity: 1;
+      text-align: left;
+    }
+    .text {
+      width: 493px;
+      color: rgba(0, 0, 0, 1);
+      padding-left: 70px;
+      padding-top: 42px;
+      font-family: inter;
+      font-weight: medium;
+      font-size: 18px;
+      opacity: 1;
+      text-align: left;
+    }
+    .btn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 0.5em 1em;
+      background: #7000de;
+      border-radius: 6px;
+      width: 12em;
+      height: 3em;
+      flex: none;
+      margin-left: 70px;
+      margin-top: 21px;
+      appearance: button;
+      text-decoration: none;
+      color: initial;
+    }
+    .btn-text {
+      font-family: raleway;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 1.3em;
+      display: flex;
+      align-items: center;
+      color: #ffffff;
+      flex: none;
+      order: 1;
+      flex-grow: 0;
+    }
+  </style>
+`;
+
+export const ACCOUNT_CREATION_INVITE_REMINDER_STYLE = `
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+    body {
+      font-size: 14px;
+    }
+    .email {
+      background: rgba(255, 255, 255, 1);
+      opacity: 1;
+      overflow: hidden;
+    }
+    .logo {
+      width: 150px;
+      height: 27px;
+      background: url("https://firebasestorage.googleapis.com/v0/b/sistering-dev.appspot.com/o/sistering-logo.png?alt=media&token=58c68ee3-848b-4fcc-902c-77f3dbf252b6");
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: cover;
+      opacity: 1;
+      margin-top: 57px;
+      margin-left: 70px;
+      overflow: hidden;
+    }
+    .title {
+      color: rgba(112, 0, 222, 1);
+      padding-left: 70px;
+      padding-top: 45px;
+      font-family: work sans;
+      font-weight: 700;
+      font-size: 24px;
+      opacity: 1;
+      text-align: left;
+    }
+    .text {
+      width: 600px;
+      color: rgba(0, 0, 0, 1);
+      padding-left: 70px;
+      padding-top: 42px;
+      font-family: inter;
+      font-weight: medium;
+      font-size: 18px;
+      opacity: 1;
+      text-align: left;
+    }
+    .btn {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 0.5em 1em;
+      background: #7000de;
+      border-radius: 6px;
+      width: 13em;
+      height: 3em;
+      flex: none;
+      margin-left: 70px;
+      margin-top: 45px;
+      appearance: button;
+      text-decoration: none;
+      color: initial;
+    }
+    .btn-text {
+      font-family: raleway;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 1.3em;
+      display: flex;
+      align-items: center;
+      color: #ffffff;
+      flex: none;
+      order: 1;
+      flex-grow: 0;
+    }
+  </style>
+`;
