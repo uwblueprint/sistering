639e15d6b1f05a0e2bd473ff714bfef25a669514
Lambert Liu <lambert0928@outlook.com>
2022-07-16 00:32:33 -0400

    Refactor AccountForm

--- frontend/src/types/api/UserType.ts
+++ frontend/src/types/api/UserType.ts
@@ -19,8 +19,10 @@ export type UserDTO = {
   id: string;
   firstName: string;
   lastName: string;
+  email: string;
   role: Role;
   phoneNumber: string | null;
+  languages: Language[];
   emergencyContactName: string | null;
   emergencyContactPhone: string | null;
   emergencyContactEmail: string | null;
@@ -29,11 +31,16 @@ export type UserDTO = {
 export type VolunteerDTO = {
   id: string;
   hireDate: Date;
-  dateOfBirth: Date | null;
+  dateOfBirth: string | null;
   pronouns: string | null;
   skills: SkillResponseDTO[];
   branches: BranchResponseDTO[];
   languages: Language[];
+};
+
+export type EmployeeUserDTO = {
+  id: string;
+  branches: BranchResponseDTO[];
 };
 
 export type CreateVolunteerDTO = {
@@ -42,9 +49,11 @@ export type CreateVolunteerDTO = {
   email: string;
   password: string;
   phoneNumber: string;
+  emergencyContactPhone: string;
   hireDate: string;
   dateOfBirth: string | null;
   skills: string[];
+  languages: string[];
   branches: BranchResponseDTO[];
 };
 
@@ -53,8 +62,19 @@ export type CreateEmployeeDTO = {
   lastName: string;
   email: string;
   phoneNumber: string;
+  emergencyContactPhone: string;
   password: string;
+  languages: string[];
   branches: BranchResponseDTO[];
 };
 
+export type EditVolunteerDTO = Omit<
+  CreateVolunteerDTO,
+  "password" | "hireDate"
+>;
+
+export type EditEmployeeDTO = Omit<CreateEmployeeDTO, "password">;
+
 export type VolunteerUserResponseDTO = UserDTO & VolunteerDTO;
+
+export type EmployeeUserResponseDTO = UserDTO & EmployeeUserDTO;
