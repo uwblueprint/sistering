5428788f611f9ac613e58c43ceaedc5f6fcba8fd
Rickson Yang <rixin.yang.2001@gmail.com>
2022-07-17 12:08:06 -0400

    Integrate branch filter

--- frontend/src/components/pages/admin/AdminHomepage.tsx
+++ frontend/src/components/pages/admin/AdminHomepage.tsx
@@ -15,6 +15,10 @@ import { Role } from "../../../types/AuthTypes";
 import { PostingResponseDTO } from "../../../types/api/PostingTypes";
 import { getPostingFilterStatus } from "../../../utils/TypeUtils";
 import { PostingFilterStatus } from "../../../types/PostingTypes";
+import {
+  BranchQueryResponse,
+  BranchResponseDTO,
+} from "../../../types/api/BranchTypes";
 
 type SimplePostingResponseDTO = Omit<PostingResponseDTO, "employees" | "type">;
 
@@ -44,6 +48,31 @@ const POSTINGS = gql`
   }
 `;
 
+const BRANCHES = gql`
+  query AdminHomepageHeader_Branches {
+    branches {
+      id
+      name
+    }
+  }
+`;
+
+const filterAdminPosting = (
+  postings: SimplePostingResponseDTO[],
+  searchFilter: string,
+  branchFilter: BranchResponseDTO | undefined,
+): SimplePostingResponseDTO[] => {
+  return postings.filter(
+    (posting) =>
+      posting.title.toLowerCase().includes(searchFilter.toLowerCase()) &&
+      (branchFilter === undefined || branchFilter.id === posting.branch.id),
+  );
+};
+
+// TODO: hook up create new posting page
+// TODO: hook up make duplicate - make dup in draft mode
+// TODO: hook up delete posting action - what can I actually delete?
+// TODO: hook up edit - do I need to create a new page?
 const AdminHomepage = (): React.ReactElement => {
   const history = useHistory();
   const { authenticatedUser } = useContext(AuthContext);
@@ -60,6 +89,10 @@ const AdminHomepage = (): React.ReactElement => {
   ]);
   const [postingStatusIndex, setPostingStatusIndex] = useState<number>(0); // refer to above for index
   const [searchFilter, setSearchFilter] = useState<string>("");
+  const [branches, setBranches] = useState<BranchResponseDTO[]>([]);
+  const [branchFilter, setBranchFilter] = useState<
+    BranchResponseDTO | undefined
+  >(undefined);
 
   const { loading, error } = useQuery(POSTINGS, {
     fetchPolicy: "cache-and-network",
@@ -72,6 +105,13 @@ const AdminHomepage = (): React.ReactElement => {
     const route = generatePath(Routes.ADMIN_SCHEDULE_POSTING_PAGE, { id });
     history.push(route);
   };
+
+  useQuery<BranchQueryResponse>(BRANCHES, {
+    fetchPolicy: "cache-and-network",
+    onCompleted: (data) => {
+      setBranches(data.branches);
+    },
+  });
 
   // update postingsByStatus 2d array
   useEffect(() => {
@@ -114,6 +154,8 @@ const AdminHomepage = (): React.ReactElement => {
           )}
           searchFilter={searchFilter}
           setSearchFilter={setSearchFilter}
+          branches={branches}
+          setBranchFilter={setBranchFilter}
         />
         <Box
           flex={1}
@@ -124,35 +166,33 @@ const AdminHomepage = (): React.ReactElement => {
         >
           <SimpleGrid columns={2} spacing={4}>
             {authenticatedUser &&
-              postingsByStatus[postingStatusIndex]
-                .filter((posting) =>
-                  posting.title
-                    .toLowerCase()
-                    .includes(searchFilter.toLowerCase()),
-                )
-                .map((posting) => (
-                  <Box key={posting.id} pb="24px">
-                    <AdminPostingCard
-                      key={posting.id}
-                      status={getPostingFilterStatus(
-                        posting.status,
-                        new Date(posting.endDate),
-                        posting.shifts,
-                      )}
-                      role={authenticatedUser.role}
-                      id={posting.id}
-                      title={posting.title}
-                      startDate={posting.startDate}
-                      endDate={posting.endDate}
-                      autoClosingDate={posting.autoClosingDate}
-                      branchName={posting.branch.name}
-                      numVolunteers={posting.numVolunteers}
-                      navigateToAdminSchedule={() =>
-                        navigateToAdminSchedule(posting.id)
-                      }
-                    />
-                  </Box>
-                ))}
+              filterAdminPosting(
+                postingsByStatus[postingStatusIndex],
+                searchFilter,
+                branchFilter,
+              ).map((posting) => (
+                <Box key={posting.id} pb="24px">
+                  <AdminPostingCard
+                    key={posting.id}
+                    status={getPostingFilterStatus(
+                      posting.status,
+                      new Date(posting.endDate),
+                      posting.shifts,
+                    )}
+                    role={authenticatedUser.role}
+                    id={posting.id}
+                    title={posting.title}
+                    startDate={posting.startDate}
+                    endDate={posting.endDate}
+                    autoClosingDate={posting.autoClosingDate}
+                    branchName={posting.branch.name}
+                    numVolunteers={posting.numVolunteers}
+                    navigateToAdminSchedule={() =>
+                      navigateToAdminSchedule(posting.id)
+                    }
+                  />
+                </Box>
+              ))}
           </SimpleGrid>
         </Box>
       </Flex>
