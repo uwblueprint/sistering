7d8d6fd7e000c93685b361e1388f73fecc6a88ed
Rickson Yang <rixin.yang.2001@gmail.com>
2022-07-23 21:18:01 -0400

    fix types

--- frontend/src/types/react-table-config.d.ts
+++ frontend/src/types/react-table-config.d.ts
@@ -52,9 +52,9 @@ import {
 declare module "react-table" {
   // take this file as-is, or comment out the sections that don't apply to your plugin configuration
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  export interface TableOptions<D extends object>
-    extends UseExpandedOptions<D>,
+  export interface TableOptions<
+    D extends object = {}
+  > extends UseExpandedOptions<D>,
       UseFiltersOptions<D>,
       UseGlobalFiltersOptions<D>,
       UseGroupByOptions<D>,
@@ -66,7 +66,7 @@ declare module "react-table" {
       // note that having Record here allows you to add anything to the options, this matches the spirit of the
       // underlying js library, but might be cleaner if it's replaced by a more specific type that matches your
       // feature set, this is a safe default.
-      Record<string, any> {}
+      Object {}
 
   export interface Hooks<D extends object = {}>
     extends UseExpandedHooks<D>,
@@ -97,9 +97,8 @@ declare module "react-table" {
       UseRowStateState<D>,
       UseSortByState<D> {}
 
-  export interface ColumnInterface<
-    D extends Record<string, unknown> = Record<string, unknown>
-  > extends UseFiltersColumnOptions<D>,
+  export interface ColumnInterface<D extends object = {}>
+    extends UseFiltersColumnOptions<D>,
       UseGlobalFiltersColumnOptions<D>,
       UseGroupByColumnOptions<D>,
       UseResizeColumnsColumnOptions<D>,
@@ -111,7 +110,7 @@ declare module "react-table" {
       UseResizeColumnsColumnProps<D>,
       UseSortByColumnProps<D> {}
 
-  export interface Cell<D extends object = {}>
+  export interface Cell<D extends object = {}, V = any>
     extends UseGroupByCellProps<D>,
       UseRowStateCellProps<D> {}
 
