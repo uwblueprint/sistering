c95792a1cd5b2d0d5dd6f3b89a0628a2ca174646
Lambert <47638847+lambo-liu@users.noreply.github.com>
2022-08-13 13:42:48 -0400

    Remove user panel mutation + fix panel UI overflow (#580)
    * Cascade user deletion to signups
    
    * Add delete mutations to profile drawer
    
    * Refetch users when user is deleted
    
    * Use refetchQueries over refetch method

--- frontend/src/components/admin/users/ProfileDrawer.tsx
+++ frontend/src/components/admin/users/ProfileDrawer.tsx
@@ -18,6 +18,7 @@ import {
 } from "@chakra-ui/react";
 
 import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
 
 import DeleteModal from "../DeleteModal";
 import { BranchResponseDTO } from "../../../types/api/BranchTypes";
@@ -43,6 +44,18 @@ type ProfileDrawerProps = {
   handleBranchMenuItemClicked: (item: BranchResponseDTO) => void;
 };
 
+const DELETE_VOLUNTEER_USER_BY_EMAIL = gql`
+  mutation deleteVolunteerUserByEmail($email: String!) {
+    deleteVolunteerUserByEmail(email: $email)
+  }
+`;
+
+const DELETE_EMPLOYEE_USER_BY_EMAIL = gql`
+  mutation deleteEmployeeUserByEmail($email: String!) {
+    deleteEmployeeUserByEmail(email: $email)
+  }
+`;
+
 const ProfileDrawer = ({
   isOpen,
   onClose,
@@ -63,78 +76,96 @@ const ProfileDrawer = ({
   handleBranchMenuItemClicked,
 }: ProfileDrawerProps): React.ReactElement => {
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const [deleteUserByEmail] = useMutation(
+    isVolunteer
+      ? DELETE_VOLUNTEER_USER_BY_EMAIL
+      : DELETE_EMPLOYEE_USER_BY_EMAIL,
+    {
+      refetchQueries: ["AdminUserManagementPage_Users"],
+    },
+  );
+
+  const onDeleteUser = async () => {
+    await deleteUserByEmail({
+      variables: {
+        email,
+      },
+    });
+    onClose();
+  };
 
   return (
     <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
       <DrawerOverlay />
       <DrawerContent>
-        <Box mt="2">
-          <Box mx="6">
-            <Text mb="2" textStyle="heading">
-              {firstName} {lastName}
-            </Text>
-            <Avatar size="lg" />
+        <DrawerBody p={0}>
+          <Box>
+            <Box m="6">
+              <Text mb="2" textStyle="heading">
+                {firstName} {lastName}
+              </Text>
+              <Avatar size="lg" />
+            </Box>
+            <Divider my="2" />
+            {isVolunteer ? (
+              <>
+                <Box mx="6">
+                  <Text textStyle="body-bold" mb="2">
+                    Skills
+                  </Text>
+                  <>
+                    {skills?.map((skill) => (
+                      <Tag variant="outline" key={skill} mb="10px" mr="10px">
+                        {skill}
+                      </Tag>
+                    ))}
+                  </>
+                </Box>
+                <Divider my="2" />
+              </>
+            ) : (
+              <></>
+            )}
+            <Box mx="6">
+              <MultiBranchSelector
+                userEmail={email}
+                branches={branches}
+                selectedBranches={selectedBranches}
+                handleBranchMenuItemClicked={handleBranchMenuItemClicked}
+              />
+            </Box>
+            <Divider my="2" />
+            <Box mx="6">
+              <Text textStyle="body-bold" mb="2">
+                Personal Information
+              </Text>
+              <HStack fontSize="14px" spacing="24px" align="start">
+                <VStack fontWeight="600" align="start" spacing="8px">
+                  <Text>First Name</Text>
+                  <Text>Last Name</Text>
+                  <Text>Pronouns</Text>
+                  <Text>Date of Birth</Text>
+                  <Text>Email Address</Text>
+                  <Text>Phone Number</Text>
+                  <Text pb="70px">Emergency Contact</Text>
+                  <Text>Language(s) Spoken</Text>
+                </VStack>
+                <VStack align="start" spacing="8px">
+                  <Text>{firstName}</Text>
+                  <Text>{lastName}</Text>
+                  <Text>{pronouns}</Text>
+                  <Text>{dateOfBirth}</Text>
+                  <Text>{email}</Text>
+                  <Text>{phoneNumber}</Text>
+                  <Text>{emergencyContactName}</Text>
+                  <Text>{emergencyContactEmail}</Text>
+                  <Text>{emergencyContactPhone}</Text>
+                  <Text>{languages.join(", ")}</Text>
+                </VStack>
+              </HStack>
+            </Box>
           </Box>
-          <Divider my="2" />
-          {isVolunteer ? (
-            <>
-              <Box mx="6">
-                <Text textStyle="body-bold" mb="2">
-                  Skills
-                </Text>
-                <>
-                  {skills?.map((skill) => (
-                    <Tag variant="outline" key={skill} mb="10px" mr="10px">
-                      {skill}
-                    </Tag>
-                  ))}
-                </>
-              </Box>
-              <Divider my="2" />
-            </>
-          ) : (
-            <></>
-          )}
-          <Box mx="6">
-            <MultiBranchSelector
-              userEmail={email}
-              branches={branches}
-              selectedBranches={selectedBranches}
-              handleBranchMenuItemClicked={handleBranchMenuItemClicked}
-            />
-          </Box>
-          <Divider my="2" />
-          <Box mx="6">
-            <Text textStyle="body-bold" mb="2">
-              Personal Information
-            </Text>
-            <HStack fontSize="14px" spacing="24px" align="start">
-              <VStack fontWeight="600" align="start" spacing="8px">
-                <Text>First Name</Text>
-                <Text>Last Name</Text>
-                <Text>Pronouns</Text>
-                <Text>Date of Birth</Text>
-                <Text>Email Address</Text>
-                <Text>Phone Number</Text>
-                <Text pb="70px">Emergency Contact</Text>
-                <Text>Language(s) Spoken</Text>
-              </VStack>
-              <VStack align="start" spacing="8px">
-                <Text>{firstName}</Text>
-                <Text>{lastName}</Text>
-                <Text>{pronouns}</Text>
-                <Text>{dateOfBirth}</Text>
-                <Text>{email}</Text>
-                <Text>{phoneNumber}</Text>
-                <Text>{emergencyContactName}</Text>
-                <Text>{emergencyContactEmail}</Text>
-                <Text>{emergencyContactPhone}</Text>
-                <Text>{languages.join(", ")}</Text>
-              </VStack>
-            </HStack>
-          </Box>
-        </Box>
-        <DrawerBody />
+        </DrawerBody>
         <Divider />
         <DrawerFooter>
           {/* this button might be removed depending on reset pw flow */}
@@ -151,7 +182,7 @@ const ProfileDrawer = ({
         body="Are you sure you want to permanently remove this user? This action cannot be undone."
         isOpen={openDeleteModal}
         onClose={() => setOpenDeleteModal(false)}
-        onDelete={() => {}}
+        onDelete={() => onDeleteUser()}
       />
     </Drawer>
   );
