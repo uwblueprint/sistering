70825b05c714841cfeab9ee6bdf520d262add81b
Rickson Yang <rixin.yang.2001@gmail.com>
2022-08-23 21:12:15 -0400

    Add missing navbars to pages

--- frontend/src/components/pages/CreateAccountPage.tsx
+++ frontend/src/components/pages/CreateAccountPage.tsx
@@ -3,6 +3,7 @@ import { Text, Button, Box, Image } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 import Sistering_Logo from "../../assets/Sistering_Logo.svg";
 import { NEW_ACCOUNT_PAGE } from "../../constants/Routes";
+import SignupNavbar from "../common/SignupNavbar";
 
 const CreateAccountPage = (): React.ReactElement => {
   const history = useHistory();
@@ -10,29 +11,34 @@ const CreateAccountPage = (): React.ReactElement => {
   const token = queryParams.get("token");
 
   return (
-    <Box width="100%" display="flex" flexDirection="row" minH="100vh">
-      <Box backgroundColor="background.white" flexGrow={4}>
-        <Box maxWidth="480px" mt="8vh" mx="auto">
-          <Image src={Sistering_Logo} alt="Sistering logo" h={32} />
-          <Box mx="43px" mb="36px">
-            <Text textStyle="display-large" marginBottom="12px">
-              Welcome to Sistering!
-            </Text>
-            <Text marginBottom="24px" textStyle="body-regular">
-              Please follow the steps to activate your sistering account. We
-              look forward to working with you.
-            </Text>
-            <Button
-              color="violet"
-              width="100%"
-              onClick={() => history.push(`${NEW_ACCOUNT_PAGE}?token=${token}`)}
-            >
-              <Text color="background.white">Continue</Text>
-            </Button>
+    <Box minH="100vh">
+      <SignupNavbar />
+      <Box width="100%" display="flex" flexDirection="row" minH="92vh">
+        <Box backgroundColor="background.white" flexGrow={4}>
+          <Box maxWidth="480px" mt="8vh" mx="auto">
+            <Image src={Sistering_Logo} alt="Sistering logo" h={32} />
+            <Box mx="43px" mb="36px">
+              <Text textStyle="display-large" marginBottom="12px">
+                Welcome to Sistering!
+              </Text>
+              <Text marginBottom="24px" textStyle="body-regular">
+                Please follow the steps to activate your sistering account. We
+                look forward to working with you.
+              </Text>
+              <Button
+                color="violet"
+                width="100%"
+                onClick={() =>
+                  history.push(`${NEW_ACCOUNT_PAGE}?token=${token}`)
+                }
+              >
+                <Text color="background.white">Continue</Text>
+              </Button>
+            </Box>
           </Box>
         </Box>
+        <Box backgroundColor="background.light" flexGrow={6} />
       </Box>
-      <Box backgroundColor="background.light" flexGrow={6} />
     </Box>
   );
 };
--- frontend/src/components/pages/admin/posting/AdminPostingDetails.tsx
+++ frontend/src/components/pages/admin/posting/AdminPostingDetails.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { VStack, Box, Container, Button, Flex } from "@chakra-ui/react";
 
 import { gql, useQuery } from "@apollo/client";
@@ -6,6 +6,14 @@ import { Redirect, useHistory, useParams } from "react-router-dom";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import PostingDetails from "../../../common/PostingDetails";
 import ErrorModal from "../../../common/ErrorModal";
+import Navbar from "../../../common/Navbar";
+import {
+  AdminNavbarTabs,
+  AdminPages,
+  EmployeeNavbarTabs,
+} from "../../../../constants/Tabs";
+import AuthContext from "../../../../contexts/AuthContext";
+import { Role } from "../../../../types/AuthTypes";
 import Loading from "../../../common/Loading";
 
 const POSTING = gql`
@@ -36,6 +44,8 @@ const POSTING = gql`
 
 const AdminPostingDetails = (): React.ReactElement => {
   const { id } = useParams<{ id: string }>();
+  const { authenticatedUser } = useContext(AuthContext);
+  const [isSuperAdmin] = useState(authenticatedUser?.role === Role.Admin);
   const {
     loading: postingLoading,
     error: postingError,
@@ -48,34 +58,40 @@ const AdminPostingDetails = (): React.ReactElement => {
   return (!postingLoading && !postingDetails) || postingError ? (
     <Redirect to="/not-found" />
   ) : (
-    <Box bg="background.light" py={7} px={10} minH="100vh">
-      {postingError && <ErrorModal />}
-      <VStack>
-        <Container pt={3} pb={6} px={0} maxW="container.xl">
-          <Flex justifyContent="flex-start">
-            <Button
-              leftIcon={<ChevronLeftIcon />}
-              variant="link"
-              onClick={() => history.goBack()}
-            >
-              Back
-            </Button>
-          </Flex>
-        </Container>
+    <Box minH="100vh">
+      <Navbar
+        defaultIndex={Number(AdminPages.AdminSchedulePosting)}
+        tabs={isSuperAdmin ? AdminNavbarTabs : EmployeeNavbarTabs}
+      />
+      <Box bg="background.light" py={7} px={10}>
+        {postingError && <ErrorModal />}
+        <VStack>
+          <Container pt={3} pb={6} px={0} maxW="container.xl">
+            <Flex justifyContent="flex-start">
+              <Button
+                leftIcon={<ChevronLeftIcon />}
+                variant="link"
+                onClick={() => history.goBack()}
+              >
+                Back
+              </Button>
+            </Flex>
+          </Container>
 
-        <Container
-          bg="background.white"
-          maxW="container.xl"
-          centerContent
-          borderRadius={10}
-        >
-          {postingLoading ? (
-            <Loading />
-          ) : (
-            <PostingDetails postingDetails={postingDetails} />
-          )}
-        </Container>
-      </VStack>
+          <Container
+            bg="background.white"
+            maxW="container.xl"
+            centerContent
+            borderRadius={10}
+          >
+            {postingLoading ? (
+              <Loading />
+            ) : (
+              <PostingDetails postingDetails={postingDetails} />
+            )}
+          </Container>
+        </VStack>
+      </Box>
     </Box>
   );
 };
